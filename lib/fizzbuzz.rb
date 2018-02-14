b7bbeea68907afc9f18d22305f94a9900f9f10ea
Agnes Donat <agi.donat@gmail.com>
2018-02-14 14:27:37 +0000

    fizzbuzz tdd

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,19 @@
 def fizzbuzz(number) 
-    'fizz'
+    
+    if number.is_a? Integer
+      if number == 0
+        return 0
+      elsif number % 3 == 0 && number % 5 == 0
+        return "FizzBuzz"
+      elsif number % 3 == 0
+        return "fizz"
+      elsif number % 5 == 0
+        return "buzz"
+      else
+        return number
+      end    
+    else
+      return "Not an integer"
+    end
+    
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,28 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-    end    
+    
+    it 'cheks if argument is an integer' do
+        expect(fizzbuzz("")).to eq 'Not an integer'
+    end   
+    
+    it 'returns "fizz" when passed multiple of 3' do
+        expect(fizzbuzz(3)).to eq "fizz"
+    end
+    
+    it 'returns "buzz" when passed multiple of 5' do
+        expect(fizzbuzz(5)).to eq "buzz"
+    end
+    
+    it 'returns "FizzBuzz" when passed multiple of 3 and 5' do
+        expect(fizzbuzz(15)).to eq "FizzBuzz"
+    end 
+    
+    it 'returns 0 when passed 0' do
+        expect(fizzbuzz(0)).to eq 0
+    end  
+    
+    it 'returns 44 when passed 44' do
+      expect(fizzbuzz(44)).to eq 44
+    end  
+    
 end
